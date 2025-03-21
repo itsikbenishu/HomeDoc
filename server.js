7b1b60cc829105523636a9fc4d38bba1c75c6190
itsikbenishu <itsikbe1@gmail.com>
2025-03-21 14:09:34 +0200

    add 0.0.0.0

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ async function testConnection() {
 testConnection();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
 
 export { postgresDB };
